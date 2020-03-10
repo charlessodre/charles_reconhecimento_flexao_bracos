86e676f5a131bb04053c227485c868614beed0d6
Charles Sodré <charlessodre@gmail.com>
2020-03-10 15:56:33 -0300

    Add files via upload
    ajustes e organização

--- reconhecimento_flexao_bracos.py
+++ reconhecimento_flexao_bracos.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-import time
+
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,30 +8,17 @@ import numpy as np
 arquivo_proto = "./pose/body/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
 arquivo_pesos = "./pose/body/mpi/pose_iter_160000.caffemodel"
 
-# Ponto utilizado no Modelo MPII
+# Pontos utilizados no Modelo MPII
 numero_pontos = 15
 pares_pontos = [[0, 1], [1, 2], [2, 3], [3, 4], [1, 5], [5, 6], [6, 7], [1, 14],
                 [14, 8], [8, 9], [9, 10], [14, 11], [11, 12], [12, 13]]
 
-# Define as cores do ponto em BGR
-cor_pontoA = (14, 201, 255)
-cor_pontoB = (255, 0, 128)
-cor_linha_esqueleto = (192, 192, 192)
-
-# Define formato da fonte do texto
-tamanho_fonte = 1
-tamanho_linha = 2
-tamanho_circulo = 3
-espessura = 1
-fonte = cv2.FONT_HERSHEY_PLAIN
-cor_fonte = (0, 255, 0)  # BGR
-
+# Dimensões do video da entrada da rede
 entrada_largura = 256
 entrada_altura = 256
 
 y_linha_base_pulso = 0
 y_linha_base_cotovelo = None
-y_linha_base_cabeca = None
 percentual_movimento_flexao = 0
 qtd_flexao_valida = 0
 
@@ -39,21 +26,22 @@ qtd_frames = 0
 pontos = None
 
 # Carrega o Video que será analisado.
-#frame = "./videos/flexao_bracos_lateral_perto.mp4"
-frame = "./videos/flexao_bracos_frontal.mp4"
-#frame = "./videos/flexao_bracos_lateral_distante.mp4"
+#video_entrada = "./videos/flexao_bracos_lateral_perto.mp4"
+video_entrada = "./videos/flexao_bracos_frontal.mp4"
+#video_entrada = "./videos/flexao_bracos_lateral_distante.mp4"
 
 
-captura = cv2.VideoCapture(frame)
-conectado, frame = captura.read()
+# Acessa o video para obter as dimensões.
+captura_inicial = cv2.VideoCapture(video_entrada)
+_, frame_inicial = captura_inicial.read()
 
-# Salva o frame com os pontos detectados.
+# Define as configurações do vídeo de saida que vai conter os pontos detectados e movimentos detectados.
 video_saida = "./flexao_bracos_saida.avi"
-gravar_video = cv2.VideoWriter(video_saida, cv2.VideoWriter_fourcc(*'XVID'), 10, (frame.shape[1], frame.shape[0]))
+gravar_video = cv2.VideoWriter(video_saida, cv2.VideoWriter_fourcc(*'XVID'), 10, (frame_inicial.shape[1], frame_inicial.shape[0]))
 
-# Carrega o modelo que já foi treinado.
+# Carrega a RN que já foi treinada e os pesos.
 modelo = cv2.dnn.readNetFromCaffe(arquivo_proto, arquivo_pesos)
-limite = 0.3  # 30%
+limite_confianca = 0.3  # 30%
 
 
 def desenha_pontos(numero_pontos, saida_RN, video_largura, video_altura):
@@ -68,9 +56,9 @@ def desenha_pontos(numero_pontos, saida_RN, video_largura, video_altura):
         x = (video_largura * ponto[0]) / largura
         y = (video_altura * ponto[1] / altura)
 
-        if confianca > limite:
-            cv2.circle(video_copia, (int(x), int(y)), 4, cor_pontoB, thickness=tamanho_circulo, lineType=cv2.FILLED)
-            cv2.putText(video_copia, "{}".format(i), (int(x), int(y)), fonte, tamanho_fonte, cor_fonte)
+        if confianca > limite_confianca:
+            cv2.circle(video_copia, (int(x), int(y)), 4, (255, 0, 128), thickness=3, lineType=cv2.FILLED)
+            cv2.putText(video_copia, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0) )
 
             pontos.append((int(x), int(y)))
         else:
@@ -81,16 +69,15 @@ def desenha_pontos(numero_pontos, saida_RN, video_largura, video_altura):
 
 def desenha_tracos(pontos, pares_pontos):
     for par in pares_pontos:
-        parteA = par[0]
-        parteB = par[1]
+        inicio_traco = par[0]
+        fim_traco = par[1]
 
-        if pontos[parteA] and pontos[parteB]:
-            cv2.line(frame, pontos[parteA], pontos[parteB], cor_linha_esqueleto, tamanho_linha, lineType=cv2.LINE_AA)
-            cv2.line(video_copia, pontos[parteA], pontos[parteB], cor_linha_esqueleto, tamanho_linha,
-                     lineType=cv2.LINE_AA)
+        if pontos[inicio_traco] and pontos[fim_traco]:
+            cv2.line(frame, pontos[inicio_traco], pontos[fim_traco], (192, 192, 192), 2, lineType=cv2.LINE_AA)
+            cv2.line(video_copia, pontos[inicio_traco], pontos[fim_traco], (192, 192, 192), 2, lineType=cv2.LINE_AA)
 
-            cv2.circle(frame, pontos[parteA], 4, cor_pontoA, thickness=espessura, lineType=cv2.FILLED)
-            cv2.circle(frame, pontos[parteB], 4, cor_pontoA, thickness=espessura, lineType=cv2.FILLED)
+            cv2.circle(frame, pontos[inicio_traco], 4, (14, 201, 255), thickness=1, lineType=cv2.FILLED)
+            cv2.circle(frame, pontos[fim_traco], 4, (14, 201, 255), thickness=1, lineType=cv2.FILLED)
 
 
 def desenha_linha_cotovelos(pontos, frame_saida, video_largura):
@@ -101,35 +88,35 @@ def desenha_linha_cotovelos(pontos, frame_saida, video_largura):
         inicio = (0, y)
         fim = (largura_frame, y)
         cv2.line(frame_saida, inicio, fim, (0, 255, 255), 1)
-        cv2.putText(frame_saida, "cabeca [{}]".format(inicio), inicio, fonte, tamanho_fonte, cor_fonte)
+        cv2.putText(frame_saida, "cabeca [{}]".format(inicio), inicio,  cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0) )
 
     if pontos[3]:
         y = pontos[3][1]
         inicio = (0, y)
         fim = (largura_frame, y)
         cv2.line(frame_saida, inicio, fim, (255, 0, 255), 1)
-        cv2.putText(frame_saida, "cotovelo direito", inicio, fonte, tamanho_fonte, cor_fonte)
+        cv2.putText(frame_saida, "cotovelo direito", inicio, fonte, cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0) )
 
     if pontos[6]:
         y = pontos[6][1]
         inicio = (0, y)
         fim = (largura_frame, y)
         cv2.line(frame_saida, inicio, fim, (0, 0, 255), 1)
-        cv2.putText(frame_saida, "cotovelo esquerdo", inicio, fonte, tamanho_fonte, cor_fonte)
+        cv2.putText(frame_saida, "cotovelo esquerdo", inicio,  cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0) )
 
     # if pontos[4]:
     #     y = pontos[4][1]
     #     inicio = (0, y)
     #     fim = (largura_frame, y)
     #     cv2.line(frame_saida, inicio, fim, (0, 0, 255), 1)
-    #     cv2.putText(frame_saida, "pulso direito", inicio, fonte, tamanho_fonte, cor_fonte)
+    #     cv2.putText(frame_saida, "pulso direito", inicio,  cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0) )
     #
     # if pontos[7]:
     #     y = pontos[7][1]
     #     inicio = (0, y)
     #     fim = (largura_frame, y)
     #     cv2.line(frame_saida, inicio, fim, (0, 0, 255), 1)
-    #     cv2.putText(frame_saida, "pulso esquerdo", inicio, fonte, tamanho_fonte, cor_fonte)
+    #     cv2.putText(frame_saida, "pulso esquerdo", inicio,  cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0) )
 
 
 def desenha_linha_base_pulso(pontos, frame_saida, video_largura, y_linha_base):
@@ -148,32 +135,14 @@ def desenha_linha_base_pulso(pontos, frame_saida, video_largura, y_linha_base):
     inicio = (0, y_linha_base)
     fim = (largura_frame, y_linha_base)
     cv2.line(frame_saida, inicio, fim, (0, 0, 255), 1)
-    cv2.putText(frame_saida, "linha base pulso", inicio, fonte, tamanho_fonte, cor_fonte)
-
-    return y_linha_base
-
-
-def desenha_linha_base_cabeca(pontos, frame_saida, video_largura, y_linha_base, y_linha_base_pulso):
-    largura_frame = int(video_largura)
-
-    cabeca = pontos[0]
-
-    if y_linha_base is None:
-        y_linha_base = y_linha_base_pulso
-
-    if cabeca and (cabeca[1] < y_linha_base and cabeca[1] < y_linha_base_pulso):
-        y_linha_base = cabeca[1]
-
-    inicio = (0, y_linha_base)
-    fim = (largura_frame, y_linha_base)
-    cv2.line(frame_saida, inicio, fim, (0, 0, 255), 1)
-    cv2.putText(frame_saida, "linha base cabeca", inicio, fonte, tamanho_fonte, cor_fonte)
+    cv2.putText(frame_saida, "linha base pulso", inicio, cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0) )
 
     return y_linha_base
 
 
 def desenha_linha_base_cotovelo(pontos, frame_saida, video_largura, y_linha_base, y_linha_base_pulso):
     largura_frame = int(video_largura)
+	
 
     cotevelo_direito = pontos[3]
     cotovelo_esquerdo = pontos[6]
@@ -190,15 +159,16 @@ def desenha_linha_base_cotovelo(pontos, frame_saida, video_largura, y_linha_base
     inicio = (0, y_linha_base)
     fim = (largura_frame, y_linha_base)
     cv2.line(frame_saida, inicio, fim, (0, 0, 255), 1)
-    cv2.putText(frame_saida, "linha base cotovelo", inicio, fonte, tamanho_fonte, cor_fonte)
+    cv2.putText(frame_saida, "linha base cotovelo", inicio, cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0) )
 
     cv2.line(frame_saida, (0, (y_linha_base - int(y_linha_base * .3))), (largura_frame, (y_linha_base - int(y_linha_base * .3))), (0, 0, 255), 1)
     return y_linha_base
 
 
-def checa_flexao(pontos, frame_saida, y_linha_base_pulso, linha_base_cotovelo, percentual_movimento_flexao,
-                 qtd_flexao_valida):
+def checa_flexao(pontos, frame_saida, y_linha_base_pulso, linha_base_cotovelo, percentual_movimento_flexao,  qtd_flexao_valida):
     cabeca = pontos[0]
+	percentual_inicio = 0.3
+	percentual_fim = 0.3
 
     if cabeca:
 
@@ -206,29 +176,26 @@ def checa_flexao(pontos, frame_saida, y_linha_base_pulso, linha_base_cotovelo, p
         # 50% do movimento completo
         if y_cabeca < y_linha_base_pulso and y_cabeca > linha_base_cotovelo:
             percentual_movimento_flexao = 0.50
-            cv2.putText(frame_saida, "Movimento {:.0%} concluido".format(percentual_movimento_flexao), (300, 35), fonte,
-                        tamanho_fonte, cor_fonte)
+            cv2.putText(frame_saida, "Movimento {:.0%} concluido".format(percentual_movimento_flexao), (300, 35), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0) )
 
         # 75% do movimento completo
         if percentual_movimento_flexao == 0.5 and y_cabeca < y_linha_base_pulso and y_cabeca < linha_base_cotovelo:
             percentual_movimento_flexao = 0.75
-            cv2.putText(frame_saida, "Movimento {:.0%} concluido".format(percentual_movimento_flexao), (300, 35), fonte,
-                        tamanho_fonte, cor_fonte)
+            cv2.putText(frame_saida, "Movimento {:.0%} concluido".format(percentual_movimento_flexao), (300, 35), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0) )
 
+		
+		y_cabeca_final = y_cabeca - (y_cabeca * percentual_fim)		
+		
         # 100% do movimento completo
-        if percentual_movimento_flexao == 0.75 and y_cabeca * 1.3 < y_linha_base_pulso and y_cabeca * 1.3 < linha_base_cotovelo:
+        if percentual_movimento_flexao == 0.75 and y_cabeca_final < y_linha_base_pulso and y_cabeca_final < linha_base_cotovelo:
             percentual_movimento_flexao = 1
             cv2.putText(frame_saida,
                         "Movimento {:.0%} concluido".format(percentual_movimento_flexao), (
-                            300, 35), fonte,
-                        tamanho_fonte, cor_fonte)
+                            300, 35), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0) )
 
         if percentual_movimento_flexao == 1:
             qtd_flexao_valida += 1
             percentual_movimento_flexao = 0
-
-    cv2.putText(frame_saida, "Movimentos Validos: {}".format(qtd_flexao_valida), (300, 20), fonte, tamanho_fonte,
-                cor_fonte)
 
     return percentual_movimento_flexao, qtd_flexao_valida
 
@@ -241,9 +208,11 @@ while (True):
     video_altura = frame.shape[0]
 
     qtd_frames += 1
-    # Processa somente os frames onde a quatidade for multiplo de 5
+	
+    # Processa somente os frames onde a quatidade for multiplo de 5 para agilizar o processamento.
     if qtd_frames % 2 == 0:
-        # Conversão do tipo da imagem
+	
+        # Conversão do tipo da imagem para o tipo aceito pela rede
         blob_entrada = cv2.dnn.blobFromImage(frame, 1.0 / 255, (entrada_largura, entrada_altura), (0, 0, 0),
                                              swapRB=False,
                                              crop=False)
@@ -251,24 +220,30 @@ while (True):
         modelo.setInput(blob_entrada)
         saida_rede = modelo.forward()
 
+		# Desenha os pontos detectados na imagem.
         pontos = desenha_pontos(numero_pontos, saida_rede, video_largura, video_altura)
 
+		# Desenha os traços conectando os pontos detectados na imagem.
         desenha_tracos(pontos, pares_pontos)
 
        # desenha_linha_cotovelos(pontos, video_copia, video_largura)
 
     if pontos is not None:
+	
+		# Desenha a linha de base do pulso que será usada como referência do "chão"
         y_linha_base_pulso = desenha_linha_base_pulso(pontos, video_copia, video_largura, y_linha_base_pulso)
-        y_linha_base_cotovelo = desenha_linha_base_cotovelo(pontos, video_copia, video_largura, y_linha_base_cotovelo,
-                                                            y_linha_base_pulso)
-
-        y_linha_base_cabeca = desenha_linha_base_cabeca(pontos, video_copia, video_largura, y_linha_base_cabeca, y_linha_base_pulso)
+		
+		# Desenha a linha de base do cotovelo que será usada como referência para validação do movimento.
+        y_linha_base_cotovelo = desenha_linha_base_cotovelo(pontos, video_copia, video_largura, y_linha_base_cotovelo, y_linha_base_pulso)
 
         if y_linha_base_cotovelo and y_linha_base_pulso:
+			# Verfica se o movimento realizado é um movimento válido.
             percentual_movimento_flexao, qtd_flexao_valida = checa_flexao(pontos, video_copia, y_linha_base_pulso,
                                                                           y_linha_base_cotovelo,
                                                                           percentual_movimento_flexao,
                                                                           qtd_flexao_valida)
+		
+		cv2.putText(video_copia, "Movimentos Validos: {}".format(qtd_flexao_valida), (300, 20), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0) )
 
     # Exibe o Video
     cv2.imshow('Video', video_copia)
@@ -280,6 +255,6 @@ while (True):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release handle to the webcam
+# Libera os recursos.
 gravar_video.release()
 cv2.destroyAllWindows()
